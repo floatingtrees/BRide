d109fe31395d42ec15652cd4d4f9fbd6d89eec13
Edward Sun <edward.sun2015@gmail.com>
2024-02-25 15:27:02 -0800

    rename login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./LandingPage.jsx";
 import SearchResult from "./SearchResult.jsx";
-import LoginPage from "./login.jsx";
+import LoginPage from "./Login.jsx";
 
 function App() {
   return (
