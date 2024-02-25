15c729fe71c82992fe05ca94f020f0ea3d5ff5fd
Edward Sun <edward.sun2015@gmail.com>
2024-02-25 15:24:56 -0800

    login stuff p3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./LandingPage.jsx";
 import SearchResult from "./SearchResult.jsx";
+import LoginPage from "./login.jsx";
 
 function App() {
   return (
--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -1,40 +1,49 @@
-import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import React, { useState } from "react";
 
 function LoginPage() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-
-  function validateCredentials(email, password) {
-
-  }
+  function validateCredentials(email, password) {}
 
   return (
     <>
-      <div className="login-credentials"> 
-      <form className="email"> <label>
-         Email: <input type="text" name="name" onChange={e => setEmail(e.target.value)}/> 
-         </label>  </form>
-      <form className="email"> <label> 
-      Password: <input type="text" name="password" onChange={e => setPassword(e.target.value)}/>
-      </label>  </form>
+      <div className="login-credentials">
+        <form className="email">
+          {" "}
+          <label>
+            Email:{" "}
+            <input
+              type="text"
+              name="name"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>{" "}
+        </form>
+        <form className="email">
+          {" "}
+          <label>
+            Password:{" "}
+            <input
+              type="text"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>{" "}
+        </form>
       </div>
       <h1>Log in to book a ride</h1>
       <div className="sign-in-button">
-        <button onClick={validateCredentials}>
-          Sign In
-        </button>
+        <button onClick={validateCredentials}>Sign In</button>
       </div>
       <p className="sign-up">
-        Don't have an account? <a href="https://orteil.dashnet.org/cookieclicker/"> Sign up</a>
+        Don't have an account?{" "}
+        <a href="https://orteil.dashnet.org/cookieclicker/"> Sign up</a>
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default LoginPage;
