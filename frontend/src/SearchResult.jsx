6c37609ac022022629a078b58dba8cc9aba7952a
Edward Sun <edward.sun2015@gmail.com>
2024-02-25 15:00:16 -0800

    home and search navigation

--- frontend/src/SearchResult.jsx
+++ frontend/src/SearchResult.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useState } from "react";
+import Navbar from "./components/Navbar";
 
 function SearchResult() {
   const [count, setCount] = useState(0);
   const [info, setInfo] = useState([Array(3).fill(null)]);
 
   //this is for temporary testing
-  
+
   //
-  
+
   function SearchElement(props) {
     return (
-      <button onClick={() => {setCount((count) => count + 1); console.log(count);}}>
+      <button
+        onClick={() => {
+          setCount((count) => count + 1);
+          console.log(count);
+        }}
+      >
         <div>car id: {props.id}</div>
         <div>car leave time:{props.time}</div>
         <div>car destination:{props.destination}</div>
@@ -22,16 +26,15 @@ function SearchResult() {
 
   return (
     <>
+      <Navbar />
       <h1>Hello, Here are Your Search Results!</h1>
       <SearchElement id={123} time={"4:5:3"} destination={"UCLA"} />
-      <SearchElement id={123} time={"4:5:3"} destination={"UCLA"} />     
+      <SearchElement id={123} time={"4:5:3"} destination={"UCLA"} />
       <div className="card">
-        <p>
-          the current count is: {count}
-        </p>
+        <p>the current count is: {count}</p>
       </div>
     </>
-  )
+  );
 }
 
 export default SearchResult;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './login.jsx'
-import './index.css'
-import LandingPage from './LandingPage.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LandingPage/>
+    <App />
   </React.StrictMode>,
-)
+);
