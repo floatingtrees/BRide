1ce664561728414f7c6719e98f7ecf2772b147b1
Edward Sun <edward.sun2015@gmail.com>
2024-03-04 21:30:14 -0800

    no css files other than index pls, we are using tailwind

--- frontend/src/Signup.jsx
+++ frontend/src/Signup.jsx
@@ -1,89 +1,85 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
-import './Signup.css'
 
 function SignUpPage() {
-
   const [email, setEmail] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [password, setPassword] = useState("");
 
   function ConfirmPasswords() {
-    const same = (confirmPassword === password);
-    if(confirmPassword.length === 0) return;
-    if(!same) {
-        return (<h1>The passwords are not the same!</h1>);
-    }
-    else {
-        return (null);
+    const same = confirmPassword === password;
+    if (confirmPassword.length === 0) return;
+    if (!same) {
+      return <h1>The passwords are not the same!</h1>;
+    } else {
+      return null;
     }
   }
   const validateCredentials = async (e) => {
-    try { 
-    const response = await fetch('/create/account', {
-        method: 'POST',
+    try {
+      const response = await fetch("/create/account", {
+        method: "POST",
         headers: {
-          'Content-Type': 'http://localhost:8000/application/json',
+          "Content-Type": "http://localhost:8000/application/json",
         },
-        body: JSON.stringify({ 'username' : username, 'password' : password})})
+        body: JSON.stringify({ username: username, password: password }),
+      });
 
-    
-    if (response.ok) {
-      console.log("WORKS")
-    }
-
+      if (response.ok) {
+        console.log("WORKS");
+      }
     } catch (error) {
-      console.log("broken")
+      console.log("broken");
     }
-    
-    return;
-  }
-  return (
-  <>
-    <Navbar />
 
-    <div className="login-credentials">
-      <form className="email">
-        {" "}
-        <label>
-          New Email:{" "}
-          <input
-            type="text"
-            name="name"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>{" "}
-      </form>
-      <form className="email">
-        {" "}
-        <label>
-          New Password:{" "}
-          <input
-            type="text"
-            name="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>{" "}
-      </form>
-      <form className="email">
-        {" "}
-        <label>
-          Confirm New Password:{" "}
-          <input
-            type="text"
-            name="password"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </label>{" "}
-      </form>
-    </div>
-    <ConfirmPasswords />
-    <h1>Sign Up to create an account!</h1>
-    <div className="sign-in-button">
-      <button onClick={validateCredentials}>Sign Up!</button>
-    </div>
-  </>
-  )
+    return;
+  };
+  return (
+    <>
+      <Navbar />
+
+      <div className="login-credentials">
+        <form className="email">
+          {" "}
+          <label>
+            New Email:{" "}
+            <input
+              type="text"
+              name="name"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>{" "}
+        </form>
+        <form className="email">
+          {" "}
+          <label>
+            New Password:{" "}
+            <input
+              type="text"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>{" "}
+        </form>
+        <form className="email">
+          {" "}
+          <label>
+            Confirm New Password:{" "}
+            <input
+              type="text"
+              name="password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </label>{" "}
+        </form>
+      </div>
+      <ConfirmPasswords />
+      <h1>Sign Up to create an account!</h1>
+      <div className="sign-in-button">
+        <button onClick={validateCredentials}>Sign Up!</button>
+      </div>
+    </>
+  );
 }
 
 export default SignUpPage;
