1bd475ac5eea8f10d09a8498497457114412c137
Fuyi-Yang <fyyang.research@gmail.com>
2024-03-09 11:25:27 -0800

    Revert "slightly fixed Footer.jsx"
    This reverts commit f6585da7e8ea06e74ca4395f7dcbfc9928c73c2b.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,19 +3,22 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#8BB8E8] rounded-lg shadow m-4 ">
-      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-white sm:text-center ">
+    <footer class="bg-[#8BB8E8] rounded-lg shadow m-4 ">
+      <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span class="text-sm text-white sm:text-center ">
           © 2024{" "}
-          <a href="https://samueli.ucla.edu/people/paul-eggert/" class="hover:underline">
+          <a
+            href="https://samueli.ucla.edu/people/paul-eggert/"
+            class="hover:underline"
+          >
             BRide.™
           </a>
           . All Rights Reserved.
         </span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0">
+        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0">
           <li>
-            <a href="#" className="hover:underline me-4 md:me-6">
-              Home
+            <a class="hover:underline me-4 md:me-6">
+              <Link to="/">Home</Link>
             </a>
           </li>
           <li>
