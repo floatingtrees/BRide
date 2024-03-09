df668da834e05d27876a807c8f7b018e39b73128
Fuyi-Yang <fyyang.research@gmail.com>
2024-03-09 14:41:11 -0800

    Edward HELP~~~

--- frontend/src/ScooterSelect.jsx
+++ frontend/src/ScooterSelect.jsx
@@ -1,15 +1,23 @@
 import {React} from "react";
 import img from "./assets/scooter.jpg";
+import ChooseSeat from "./components/ChooseSeat";
 
-function ScooterSelectPage() {
+function ScooterSelectPage(prop) {
     return (
         <>
           <h1 className="md:text-4xl sm:text-3xl text-2xl fond-bold py-2">
             Select Your Scooter Seat!
           </h1>
-          <div className="h-96 w-full">
-            <img src={img} alt="My Image" className="object-contain h-full w-full" />
-         </div>
+        <div className="flex justify-center items-center pt-24">
+          <div className="flex space-x-32">
+            <button className="w-32 h-32 rounded-full bg-blue-500 text-white font-semibold">
+                Back Seat
+            </button>
+            <button className="w-32 h-32 rounded-full bg-green-500 text-white font-semibold ">
+                Front Seat
+            </button>
+          </div>
+        </div>
         </>
     );
 }
