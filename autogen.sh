e94657df006afe309cee9c9877ba3fb2b1df146c
floatingtrees <96200235+floatingtrees@users.noreply.github.com>
2024-03-09 13:43:34 -0800

    shell script works now

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,18 @@
-python3 backend/server.py
-cd frontend
-npm run dev
+#!/bin/bash
+
+cd backend
+python -m venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+
+cd ../frontend
+npm install
+cd ..
+
+
+cd frontend && npm run dev &
+
+cd backend && python3 server.py
+
+# Change directory to frontend and run the npm script
+
