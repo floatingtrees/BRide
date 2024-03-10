2cb9e86dd8fa93b0c83624f8a3c210c94a298df6
Fuyi-Yang <fyyang.research@gmail.com>
2024-03-09 17:53:30 -0800

    added default card on search result

--- frontend/src/SearchResult.jsx
+++ frontend/src/SearchResult.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-
+import img from "./assets/Card.jpg";
+import default_img from "./assets/default_card.jpg";
 import Navbar from "./components/Navbar";
 import { useLocation, Link } from "react-router-dom";
 import { Grid } from "@mui/material";
@@ -24,27 +25,31 @@ function SearchResultPage() {
     <>
       <div className="pt-24">
         <Searchbar />
+        <h1 className="flex justify-center text-[#0380fc] md:text-7xl sm:tetx-6xl text-4xl font-bold md:py-6">
+          Your Search Results
+        </h1>
         <Container maxWidth="lg">
           <Grid
             container
             spacing={5}
-            style={{ marginTop: "20px" }}
+            style={{ marginTop: "-15px" }}
             columns={16}
           >
             <ResultCard
-              name="Bob John"
-              location="De Neve Plaza"
-              time="10:10 AM"
+              name="New Scooter!!!"
+              img={default_img}
             />
             <ResultCard
               name="Bob John"
               location="De Neve Plaza"
               time="10:10 AM"
+              img={img}
             />
             <ResultCard
               name="Bob John"
               location="De Neve Plaza"
               time="10:10 AM"
+              img={img}
             />
           </Grid>
         </Container>
