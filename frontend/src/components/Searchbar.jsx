46872f4a25501f5a912ed22e4c0385c20fd30935
Edward Sun <edward.sun2015@gmail.com>
2024-03-09 20:34:12 -0800

    fixed search bar button and sizes

--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -9,7 +9,7 @@ function Searchbar() {
   const [time, setTime] = useState("");
   const [start, setStart] = useState("");
   const [end, setEnd] = useState("");
-  const dataToSend = {start:"", end:"", date:"", time:""}
+  const dataToSend = { start: "", end: "", date: "", time: "" };
   const navigate = useNavigate();
 
   function retrieveData() {
@@ -19,58 +19,59 @@ function Searchbar() {
     // dataToSend.time=data.time.substring(data.time.length - 5);
     // dataToSend.date=data.time.substring(0,data.time.length-5);
 
-    dataToSend.start=start;
-    dataToSend.end=end;
-    dataToSend.time=time;
-    dataToSend.date=date;
+    dataToSend.start = start;
+    dataToSend.end = end;
+    dataToSend.time = time;
+    dataToSend.date = date;
   }
 
   const handleSubmit = async (e) => {
-    
     e.preventDefault();
-     
+
     const timeRegex = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
-    console.log(timeRegex)
+    console.log(timeRegex);
 
     // Allow empty value to let user clear the input or type from scratch
     if (!timeRegex.test(time)) {
-      alert("Time must be in the format of HH:MM, such as 09:30")
+      alert("Time must be in the format of HH:MM, such as 09:30");
       return;
-
     }
-    const dateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/(19|20)\d\d$/;
-    console.log(dateRegex)
+    const dateRegex =
+      /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/(19|20)\d\d$/;
+    console.log(dateRegex);
 
     // Allow empty value to let user clear the input or type from scratch
     if (!dateRegex.test(date)) {
-      alert("Date must be in the format of MM/DD/YYYY, such as 4/1/2024")
+      alert("Date must be in the format of MM/DD/YYYY, such as 4/1/2024");
       return;
-
     }
     const response = await fetch("http://localhost:8000/request", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ 'startLocation': start, 'endLocation': end, 'time': date + time}),
-      })
-    .then(response => response.json())
-    .then(data => {
-      if (data.success === "True") {
-        retrieveData(data);
-      }
-      console.log(dataToSend);
-    });
-    navigate('/search', {state: dataToSend})
-
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        startLocation: start,
+        endLocation: end,
+        time: date + time,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success === "True") {
+          retrieveData(data);
+        }
+        console.log(dataToSend);
+      });
+    navigate("/search", { state: dataToSend });
   };
 
   return (
     <>
-      <div className="flex justify-center items-center h-24 max-w-[1240px] px-6 mx-auto rounded-2xl p-4 my-4 bg-white">
+      <div className="flex justify-center items-center h-24  px-6 mx-auto rounded-2xl p-4 my-4 bg-white w-full">
         <form className="bg-white rounded" onSubmit={handleSubmit}>
           <div className="flex flex-col md:flex-row md:space-x-2">
-          <LabelInputContainer>
+            <LabelInputContainer>
               <Label htmlFor="firstname">Arrival Location</Label>
               <Input
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -112,7 +113,7 @@ function Searchbar() {
               />
             </LabelInputContainer>
             <button
-              className="mt-3.5 group/btn w-32 text-white h-10 font-bold bg-[#2774AE] btn overflow-hidden rounded-xl uppercase duration-300 hover:-translate-y-2 hover:shadow-md"
+              className="mt-3.5 group/btn w-96 text-white h-10 font-bold bg-[#2774AE] btn overflow-hidden rounded-xl uppercase duration-300 hover:-translate-y-2 hover:shadow-lg"
               type="submit"
             >
               Go
