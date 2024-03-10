a8e62cc636bff674eca54673cf2ab49f23df56aa
Edward Sun <edward.sun2015@gmail.com>
2024-03-09 22:09:32 -0800

    fix auto gen

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd backend
-python -m venv venv2
+python3 -m venv venv2
 source venv2/bin/activate
 pip3 install fastapi
 pip3 install uvicorn
@@ -10,6 +10,15 @@ cd ../frontend
 npm install
 cd ..
 
+if [ -d "backend/data/" ]; then
+	echo "Data Directory Found."
+else
+	cd backend
+	mkdir data
+	cd data
+	touch database.txt
+	cd ../..
+fi
 
 cd frontend && npm run dev &
 
