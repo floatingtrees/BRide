c78442cc9e3def3a79aac6ff2cbad3aa59c4b986
Fuyi-Yang <fyyang.research@gmail.com>
2024-03-09 18:31:57 -0800

    Revert "added text to the first two landing page boxes along with making the blinking thing gone"
    This reverts commit 47ee3b1b0b7cc4483ef34b3b29b6c2b6c7fe3a69.

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import scooter from "../assets/First.png";
+import scooter from "../assets/Scooter.gif";
 
 const About = () => {
-  const paragraphStyle = {
-    caretColor: 'transparent',
-  };
   return (
     <div className="w-full py-16 px-4">
       <div className="mx-auto grid md:grid-cols-2">
@@ -14,13 +11,15 @@ const About = () => {
           alt="scooter"
         ></img>
         <div className="flex flex-col justify-center">
-          <p className=" uppercase font-bold"style={paragraphStyle}>Share Scooter Rides!</p>
-          <h1 className="md:text-4xl sm:text-3xl text-2xl fond-bold py-2" style={paragraphStyle}>
-            Want to arrive to class within minutes?
+          <p className=" uppercase font-bold">Share Scooter Rides!</p>
+          <h1 className="md:text-4xl sm:text-3xl text-2xl fond-bold py-2">
+            Cock and Balls
           </h1>
-          <p style={paragraphStyle}>
-            Never have to run to class again. Have you ever left your room knowing you won't make it to class on time, not wanting to pay the steep price of a scooter? BRide allows you
-            to share a scooter with another student running late to class.
+          <p className="">
+            i love cock oh boy i love cocks i love cock oh boy i love cocks i
+            love cock oh boy i love cocks i love cock oh boy i love cocks i love
+            cock oh boy i love cocks i love cock oh boy i love cocks i love cock
+            oh boy i love cocks i love cock oh boy i love cocks
           </p>
         </div>
       </div>
