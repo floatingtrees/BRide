6da0e9f1de06065d3fd6715a8def75b5d6f8bfea
Edward Sun <edward.sun2015@gmail.com>
2024-03-13 16:58:01 -0700

    fixed landing

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -20,29 +20,27 @@ const About = () => {
   return (
     <div className="w-full py-16 px-4">
       <div className="mx-auto grid md:grid-cols-2 gap-8">
-        <div className="flex flex-col justify-center pl-12">
-          <img
-            className="w-[500px] mx-auto my-4 rounded-2xl shadow-xl"
-            src={map}
-            alt="map"
-          ></img>
-          <div className="flex flex-col justify-center">
-            <p className=" uppercase font-bold" style={paragraphStyle}>
-              In a rush?
-            </p>
-            <h1
-              className="md:text-4xl sm:text-3xl text-2xl fond-bold py-2"
-              style={headingStyle}
-            >
-              Arrive to class within minutes!
-            </h1>
-            <p style={paragraphStyle}>
-              Never have to run to class again. Have you ever left your room
-              knowing you won't make it to class on time, not wanting to pay the
-              steep price of a scooter? BRide allows you to share a scooter with
-              another student running late to class, cutting costs in half.
-            </p>
-          </div>
+        <img
+          className="w-[500px] mx-auto my-4 rounded-2xl shadow-xl"
+          src={map}
+          alt="map"
+        ></img>
+        <div className="flex flex-col justify-center">
+          <p className=" uppercase font-bold" style={paragraphStyle}>
+            In a rush?
+          </p>
+          <h1
+            className="md:text-4xl sm:text-3xl text-2xl fond-bold py-2"
+            style={headingStyle}
+          >
+            Arrive to class within minutes!
+          </h1>
+          <p style={paragraphStyle}>
+            Never have to run to class again. Have you ever left your room
+            knowing you won't make it to class on time, not wanting to pay the
+            steep price of a scooter? BRide allows you to share a scooter with
+            another student running late to class, cutting costs in half.
+          </p>
         </div>
       </div>
     </div>
--- frontend/src/components/HowTo.jsx
+++ frontend/src/components/HowTo.jsx
@@ -21,24 +21,26 @@ const HowTo = () => {
     <div className="w-full bg-[#8BB8E8] px-4 py-16 text-white">
       <div className="mx-auto grid md:grid-cols-2 gap-8">
         <div className="flex flex-col justify-center pl-12">
-          <p
-            className=" font-bold uppercase text-[#e5e5e5]"
-            style={paragraphStyle}
-          >
-            How to look for a match?
-          </p>
-          <h1
-            className="fond-bold py-2 text-2xl sm:text-3xl md:text-4xl"
-            style={headingStyle}
-          >
-            Fast and easy!
-          </h1>
-          <p className="" style={paragraphStyle}>
-            Finding a match is simple. If it's your first time using BRide,
-            create an account using your name email and contract info, then
-            enter your trip and select your spot on the scooter. BRide will
-            notify you as soon as a match is found.
-          </p>
+          <div className="flex flex-col justify-center">
+            <p
+              className=" font-bold uppercase text-[#e5e5e5]"
+              style={paragraphStyle}
+            >
+              How to look for a match?
+            </p>
+            <h1
+              className="fond-bold py-2 text-2xl sm:text-3xl md:text-4xl"
+              style={headingStyle}
+            >
+              Fast and easy!
+            </h1>
+            <p className="" style={paragraphStyle}>
+              Finding a match is simple. If it's your first time using BRide,
+              create an account using your name email and contract info, then
+              enter your trip and select your spot on the scooter. BRide will
+              notify you as soon as a match is found.
+            </p>
+          </div>
         </div>
         <img
           className="mx-auto my-4 w-[500px] rounded-2xl shadow-xl"
